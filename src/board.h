bab82f06c054502bce2afdc2d408b058116a651b
Занорин Владислав Викторович <ip811s09@awp06.csc.local>
2019-05-29 10:48:23 +0700

    dockabible

---
+++ src/board.h
@@ -0,0 +1,17 @@
+#ifndef BOARD_H
+
+#define BOARD_H
+
+void check(char deck[8][8], int test);
+
+int check_figure(char deck[8][8], int move[4]);
+
+int input_check(char str[4]);
+
+int PAWN(char deck[8][8], int move[4]);
+
+int pawn(char deck[8][8], int move[4]);
+
+void make_move(char deck[8][8], int move[]);
+
+#endif
